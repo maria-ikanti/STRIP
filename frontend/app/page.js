7b35621fd3d4f4413d575d5f66fc61c9c657635b
maria-ikanti <maria@nion.fr>
2024-04-03 14:29:16 +0200

    fff

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { useAccount } from "wagmi";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; 
 
@@ -5,15 +7,11 @@ export default function Home() {
 
   // On récupère l'adresse du compte qui est connecté à la DApp
   // On récupère aussi s'il y a qqn connecté ou pas
-  const { address, isConnected } = useAccount();
+  //const { address, isConnected } = useAccount();
   
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='*' element={<Navigate to='/home' />} />
-       </Routes>
-     </BrowserRouter>
+
     </>
   );
 }
