7046f85e67f2be4c43f6509e2444f2b90da1c748
maria-ikanti <maria@nion.fr>
2024-04-03 14:24:26 +0200

    Remove use client from main page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,3 @@
-'use client';
-
 import { useAccount } from "wagmi";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; 
 
@@ -8,7 +6,7 @@ export default function Home() {
   // On récupère l'adresse du compte qui est connecté à la DApp
   // On récupère aussi s'il y a qqn connecté ou pas
   const { address, isConnected } = useAccount();
-
+  
   return (
     <>
       <BrowserRouter>
